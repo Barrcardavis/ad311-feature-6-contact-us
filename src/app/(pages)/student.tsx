32e5e08a39710418ca5b28535fa696e9f871d7d1
Fathiya Yoosef <155487698+Dhool143@users.noreply.github.com>
2026-03-09 10:44:19 -0700

    Save student file changs

--- src/app/(pages)/student.tsx
+++ src/app/(pages)/student.tsx
@@ -3,12 +3,11 @@ import { View, Text } from 'react-native';
 
 export default function StudentPage() {
   return (
-    <View style={{ flex: 1}}>
-      <Text style={{textAlign: "center" , marginTop: 20, justifyContent: "center",  
-        alignItems: "center",}}></Text>
-        <View style={{width: "100%", alignItems: "center"}}>
-      
-      </View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+
+      <Text>Welcome to the Student Page!</Text>
+
     </View>
+
   );
 }
